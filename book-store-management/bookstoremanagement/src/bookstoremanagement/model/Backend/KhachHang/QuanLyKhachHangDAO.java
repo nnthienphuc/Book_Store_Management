faba7e538c7951dbe532076a63e8b0aed95c853e
Phuc Ngoc Thien Nguyen <nnthienphuc@gmail.com>
2024-02-03 17:46:57 +0700

    success modify KhachHang package

--- book-store-management/bookstoremanagement/src/bookstoremanagement/model/Backend/KhachHang/QuanLyKhachHangDAO.java
+++ book-store-management/bookstoremanagement/src/bookstoremanagement/model/Backend/KhachHang/QuanLyKhachHangDAO.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package bookstoremanagement.model.Backend.ConnectDB.ConnectionDB;
+package bookstoremanagement.model.Backend.KhachHang;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
-
 import bookstoremanagement.model.Backend.ConnectDB.ConnectionDB;
 
 /**
