d2453473ccd72ed7b4ec83103046aabad10d1fe4
Phuc Ngoc Thien Nguyen <nnthienphuc@gmail.com>
2024-02-03 15:13:57 +0700

    Create packages and add HoldPackage class to upload these packages  to Github

---
+++ book-store-management/bookstoremanagement/src/bookstoremanagement/model/Backend/HoaDonNhap/HoldPackage.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookstoremanagement.model.Backend.HoaDonNhap;
+
+import bookstoremanagement.frames.*;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class HoldPackage {
+    
+}
