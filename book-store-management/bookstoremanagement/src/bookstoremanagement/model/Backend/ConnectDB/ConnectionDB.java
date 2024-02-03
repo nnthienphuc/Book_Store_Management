8fe0a999f5c2931a7f619bd453d1e600fbc02d1c
Phuc Ngoc Thien Nguyen <nnthienphuc@gmail.com>
2024-02-03 15:39:29 +0700

    Create ConnectionDB file

---
+++ book-store-management/bookstoremanagement/src/bookstoremanagement/model/Backend/ConnectDB/ConnectionDB.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookstoremanagement.model.Backend.ConnectDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author nnthienphuc
+ */
+public class ConnectionDB {
+    
+}
